602da46761517c42d8df44cf95e900f91b4bdd23
Jon Gucciardi <jon.gucciardi@catapultsystems.com>
2019-09-14 16:40:23 -0500

    added more comments

--- src/Components/ProfileInfo/index.jsx
+++ src/Components/ProfileInfo/index.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import StoreState from "../../Store/state";
 
+
+// the class component (although it could be a functional component) will only render AFTER the loading component is complete
+// and it won't try and render any data unless the prop user exists and has the fullName property filled in
+// otherwise it will notify the user, something went wrong and we couldn't fetch their info at this time.
+
 class ProfileInfo extends Component {
   render() {
     return (
@@ -9,11 +14,17 @@ class ProfileInfo extends Component {
         <div>
           <p style={{textAlign: 'center'}}> Profile Info Component</p>
         </div>
+        {this.props.user && this.props.user.fullName ?
         <div>
           <p> First Name: {this.props.user.firstName}</p>
           <p> Last Name: {this.props.user.lastName}</p>
           <p> Email: {this.props.user.email}</p>
         </div>
+          :
+          <div>
+            <p>Error in Pulling down user Info</p>
+          </div>
+        }
       </div>
     )
   }
